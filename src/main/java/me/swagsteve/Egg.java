eaae03b33f19b76221485c9fdc74a10c9b7e60fe
BuildTools <unconfigured@null.spigotmc.org>
2022-02-12 14:22:13 +0000

    Set custom egg chances update

--- src/main/java/me/swagsteve/Egg.java
+++ src/main/java/me/swagsteve/Egg.java
@@ -8,12 +8,25 @@ import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 
+import java.util.Random;
+
 public final class Egg extends JavaPlugin implements Listener {
+
+    //Instance
+    private static Egg instance;
+    public static Egg getInstance(){
+        return instance;
+    }
 
     @Override
     public void onEnable() {
+        getCommand("egg-reload").setExecutor(new ReloadCommand());
+        instance = this;
         System.out.println("[100% Egg] Has Been Enabled!");
         getServer().getPluginManager().registerEvents(this,this);
+        this.getConfig().options().copyDefaults();
+        this.getConfig().addDefault("Setup.EggChance-Of-100", 100);
+        saveDefaultConfig();
     }
 
     @EventHandler
@@ -28,11 +41,15 @@ public final class Egg extends JavaPlugin implements Listener {
     public void onProjectileHit(ProjectileHitEvent event) {
         if (event.getEntity().getType() == EntityType.EGG) {
 
-            //Summon Chicken At Egg Land Spot
-            org.bukkit.entity.Egg egg = (org.bukkit.entity.Egg) event.getEntity();
-            @SuppressWarnings("deprecation")
-            Player player = (Player) egg.getShooter();
-            egg.getLocation().getWorld().spawn(egg.getLocation(), Chicken.class);
+            Random r = new Random();
+            int chance = r.nextInt(100);
+            if (chance < getConfig().getInt("Setup.EggChance-Of-100")) {
+                //Summon Chicken At Egg Land Spot
+                org.bukkit.entity.Egg egg = (org.bukkit.entity.Egg) event.getEntity();
+                @SuppressWarnings("deprecation")
+                Player player = (Player) egg.getShooter();
+                egg.getLocation().getWorld().spawn(egg.getLocation(), Chicken.class);
+            }
         }
     }
 
@@ -40,5 +57,6 @@ public final class Egg extends JavaPlugin implements Listener {
     public void onDisable() {
         // Plugin startup logic
         System.out.println("[100% Egg] Has Been Disabled!");
+        saveConfig();
     }
 }
