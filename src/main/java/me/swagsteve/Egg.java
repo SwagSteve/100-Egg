674148f2bdc42122414a4a286ee4f1007c3adfc2
BuildTools <unconfigured@null.spigotmc.org>
2023-06-12 20:52:40 +0100

    - 1.20 support - Added option to make chickens adults in config - Multi-version support (in 1 jar file!) - Simplified config.yml - Better config redundancy logic

--- src/main/java/me/swagsteve/Egg.java
+++ src/main/java/me/swagsteve/Egg.java
@@ -6,8 +6,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.entity.Player;
 
+import java.io.File;
 import java.util.Random;
 
 public final class Egg extends JavaPlugin implements Listener {
@@ -18,23 +18,42 @@ public final class Egg extends JavaPlugin implements Listener {
         return instance;
     }
 
+    //Config
+    public static Integer egg_percentage;
+    public static Boolean is_adult;
+
     @Override
     public void onEnable() {
-        getCommand("egg-reload").setExecutor(new ReloadCommand());
+
+        //Instance
         instance = this;
-        System.out.println("[100% Egg] Has Been Enabled!");
+
+        //Commands
+        getCommand("egg-reload").setExecutor(new ReloadCommand());
+
+        //Events
         getServer().getPluginManager().registerEvents(this,this);
-        this.getConfig().options().copyDefaults();
-        this.getConfig().addDefault("Setup.EggChance-Of-100", 100);
-        saveDefaultConfig();
+
+        //Config
+        File tempConfig = new File(getDataFolder(), "config.yml");
+        if (!tempConfig.exists()) {
+            getLogger().info("Generating config...");
+            saveDefaultConfig();
+            reloadConfig();
+        }
+
+        //Cache config
+        egg_percentage = getConfig().getInt("Setup.egg-percentage");
+        is_adult = getConfig().getBoolean("Setup.is-adult");
+
+        //Enabled message
+        getLogger().info("Enabled!");
     }
 
     @EventHandler
     public void EggThrowEvent(PlayerEggThrowEvent e) {
-
-        //Cancel Regular Hatching, Removes Duplicates
+        //Cancel regular hatching, removes duplicates
         e.setHatching(false);
-
     }
 
     @EventHandler
@@ -43,20 +62,29 @@ public final class Egg extends JavaPlugin implements Listener {
 
             Random r = new Random();
             int chance = r.nextInt(100);
-            if (chance < getConfig().getInt("Setup.EggChance-Of-100")) {
+            if (chance < egg_percentage) {
+
                 //Summon Chicken At Egg Land Spot
                 org.bukkit.entity.Egg egg = (org.bukkit.entity.Egg) event.getEntity();
-                @SuppressWarnings("deprecation")
-                Player player = (Player) egg.getShooter();
-                egg.getLocation().getWorld().spawn(egg.getLocation(), Chicken.class);
+                Chicken chicken = (Chicken) egg.getLocation().getWorld().spawnEntity(egg.getLocation(), EntityType.CHICKEN);
+
+                //Set chicken age
+                if (is_adult) {
+                    chicken.setAdult();
+                } else {
+                    chicken.setBaby();
+                }
             }
         }
     }
 
     @Override
     public void onDisable() {
-        // Plugin startup logic
-        System.out.println("[100% Egg] Has Been Disabled!");
+
+        //Disable message
+        getLogger().info("Disabled!");
+
+        //Save config
         saveConfig();
     }
 }
